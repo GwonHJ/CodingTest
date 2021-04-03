361b42f417cbc93e1d1a695282aaff9060601726
GwonHJ <45057466+GwonHJ@users.noreply.github.com>
2021-04-04 02:18:29 +0900

    구현, 자료구조, 문자열, 해시를 사용한 집합과 맵

---
+++ 백준/자료구조1/2002_추월.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	int N = Integer.parseInt(br.readLine());
+    	
+    	
+    	HashMap<String, Integer> in = new HashMap<>();
+    	
+    	for(int i =0; i<N; i++) {
+    		String car = br.readLine();
+    		in.put(car,i);
+    	}
+    	
+    	int[] out = new int[N];
+    	
+    	for(int i = 0; i<N; i++) {
+    		String car = br.readLine();
+    		out[i] = in.get(car);
+    	}
+    	
+    	int cnt = 0;
+    	for(int i= 0; i<N-1; i++) {
+    		for(int j = i+1; j<N; j++) {
+    			if(out[i]>out[j]) {
+    				cnt++;
+    				break;
+    			}
+    		}
+    	}
+    	
+    	System.out.println(cnt);
+    }   
+    
+}
