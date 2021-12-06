cf66d1ae0f0cffcf84b26964d125de8d1b0a6cf6
GwonHJ <45057466+GwonHJ@users.noreply.github.com>
2021-12-06 22:33:02 +0900

    Update 스택으로 큐 구현.cpp

--- 프로그래머스/COS/스택으로 큐 구현.cpp
+++ 프로그래머스/COS/스택으로 큐 구현.cpp
@@ -15,16 +15,8 @@ int func_a(vector<int> &stack) {
 }
 
 void func_b(vector<int> &stack1, vector<int> &stack2) {
-    while(!func_
-c
-(
-stack1
-)) {
-        int item = func_
-a
-(
-stack1
-);
+    while(!func_c(stack1)) {
+        int item = func_a(stack1);
         stack2.push_back(item);
     }
 }
@@ -34,22 +26,10 @@ bool func_c(vector<int> stack) {
 }
 
 int solution(vector<int> stack1, vector<int> stack2) {
-    if(func_
-c
-(
-stack2
-)){
-        func_
-b
-(
-stack1, stack2
-);
+    if(func_c(stack2)){
+        func_b(stack1, stack2);
     }
-    int answer = func_
-a
-(
-stack2
-);
+    int answer = func_a(stack2);
     return answer;
 }
 
